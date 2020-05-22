b6533f15abb8fe20948aa418f949d296968b20b2
Bastien Gorissen <bastien.gorissen@gsmproductions.com>
2020-05-22 12:59:35 +0200

    Status at the end of the day - game complete

---
+++ Assets/Scripts/EnemyBehaviour.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.SceneManagement;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+
+    public GameObject target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        GetComponent<NavMeshAgent>().destination = target.transform.position;
+    }
+
+    void OnCollisionEnter(Collision collision) {
+        if (collision.collider.tag == "Player")
+            SceneManager.LoadScene("lose", LoadSceneMode.Additive);
+    }
+}
---
+++ Assets/Scripts/Timeout.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Timeout : MonoBehaviour
+{
+
+    public float time;
+
+    // Update is called once per frame
+    void Update()
+    {
+        time -= Time.deltaTime;
+        if (time <= 0f)
+            SceneManager.LoadScene(0);
+    }
+}
